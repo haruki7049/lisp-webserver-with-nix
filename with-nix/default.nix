e2d07160d3385140a5184b19111eacba4d9c2d76
haruki7049 <tontonkirikiri@gmail.com>
2024-05-27 10:30:36 +0900

    chore: Add clack

--- with-nix/default.nix
+++ with-nix/default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { inherit overlays; }
+{ pkgs ? import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz) { inherit overlays; }
 , lib ? pkgs.lib
 , cl-nix-lite ? import (builtins.fetchTarball https://github.com/hraban/cl-nix-lite/archive/c64a7994c556bab132ea19827e279dbd2b7576a3.tar.gz)
 , overlays ? [ cl-nix-lite ]
@@ -12,6 +12,7 @@ lispPackagesLite.lispDerivation rec {
   dontStrip = true;
 
   lispDependencies = with pkgs.lispPackagesLite; [
+    clack
   ];
 
   meta = with lib; {
